567e167c172d94f89a4f545939477f2d5cfcdb1d
nikishkaa <nikita.goutor@gmail.com>
2024-10-14 21:14:55 +0300

    Update

--- src/main/java/com/websystique/springmvc/dao/UserDaoImpl.java
+++ src/main/java/com/websystique/springmvc/dao/UserDaoImpl.java
@@ -13,55 +13,54 @@ import org.springframework.stereotype.Repository;
 import com.websystique.springmvc.model.User;
 
 
-
 @Repository("userDao")
 public class UserDaoImpl extends AbstractDao<Integer, User> implements UserDao {
 
-	static final Logger logger = LoggerFactory.getLogger(UserDaoImpl.class);
-	
-	public User findById(int id) {
-		User user = getByKey(id);
-		if(user!=null){
-			Hibernate.initialize(user.getUserProfiles());
-		}
-		return user;
-	}
+    static final Logger logger = LoggerFactory.getLogger(UserDaoImpl.class);
 
-	public User findBySSO(String sso) {
-		logger.info("SSO : {}", sso);
-		Criteria crit = createEntityCriteria();
-		crit.add(Restrictions.eq("ssoId", sso));
-		User user = (User)crit.uniqueResult();
-		if(user!=null){
-			Hibernate.initialize(user.getUserProfiles());
-		}
-		return user;
-	}
+    public User findById(int id) {
+        User user = getByKey(id);
+        if (user != null) {
+            Hibernate.initialize(user.getUserProfiles());
+        }
+        return user;
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<User> findAllUsers() {
-		Criteria criteria = createEntityCriteria().addOrder(Order.asc("firstName"));
-		criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);//To avoid duplicates.
-		List<User> users = (List<User>) criteria.list();
-		
-		// No need to fetch userProfiles since we are not showing them on list page. Let them lazy load. 
-		// Uncomment below lines for eagerly fetching of userProfiles if you want.
+    public User findBySSO(String sso) {
+        logger.info("SSO : {}", sso);
+        Criteria crit = createEntityCriteria();
+        crit.add(Restrictions.eq("ssoId", sso));
+        User user = (User) crit.uniqueResult();
+        if (user != null) {
+            Hibernate.initialize(user.getUserProfiles());
+        }
+        return user;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<User> findAllUsers() {
+        Criteria criteria = createEntityCriteria().addOrder(Order.asc("firstName"));
+        criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);//To avoid duplicates.
+        List<User> users = (List<User>) criteria.list();
+
+        // No need to fetch userProfiles since we are not showing them on list page. Let them lazy load.
+        // Uncomment below lines for eagerly fetching of userProfiles if you want.
 		/*
 		for(User user : users){
 			Hibernate.initialize(user.getUserProfiles());
 		}*/
-		return users;
-	}
+        return users;
+    }
 
-	public void save(User user) {
-		persist(user);
-	}
+    public void save(User user) {
+        persist(user);
+    }
 
-	public void deleteBySSO(String sso) {
-		Criteria crit = createEntityCriteria();
-		crit.add(Restrictions.eq("ssoId", sso));
-		User user = (User)crit.uniqueResult();
-		delete(user);
-	}
+    public void deleteBySSO(String sso) {
+        Criteria crit = createEntityCriteria();
+        crit.add(Restrictions.eq("ssoId", sso));
+        User user = (User) crit.uniqueResult();
+        delete(user);
+    }
 
 }
